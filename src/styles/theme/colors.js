4ba809cde1f3b03de2e0ff5c79e336fbb18abd1f
lsy20140 <lsy20140@naver.com>
2024-03-21 22:55:50 +0900

    style: theme colors.js 추가

---
+++ src/styles/theme/colors.js
@@ -0,0 +1,20 @@
+export const colors = {
+  gray: {
+    50: '#F7FBFF',
+    100: '#F3F3F3',
+    200: '#DEDEDE',
+    300: '#CCCCCC',
+    400: '#9E9E9E',
+    500: '#999999',
+    600: '#7E8489',
+    700: '#666666',
+    800: '#535353',
+    900: '#222222'
+  },
+  primary: '#FD835F',
+  secondary: '#39375C',
+  light: '#F0EAE0',
+  empty: '#DEDEDE'
+
+} 
+
